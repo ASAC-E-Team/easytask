a3e338cc03fd69047094576d5a715e507b6616ec
JinGyuGwak <wlsrb2695@naver.com>
2023-02-22 12:58:22 +0900

    develop init

---
+++ easytask/src/main/java/com/easytask/easytask/common/exception/BaseException.java
@@ -0,0 +1,10 @@
+package com.easytask.easytask.common.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseException extends RuntimeException {
+    public BaseException(String message) { super(message); }
+}
---
+++ easytask/src/main/java/com/easytask/easytask/src/user/entity/User.java
@@ -0,0 +1,20 @@
+package com.easytask.easytask.src.user.entity;
+
+import com.easytask.easytask.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @Column(name = "userId", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
