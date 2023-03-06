343ad9310df52361248300993831dac3f8381b02
CJH <80333155+JH19980421@users.noreply.github.com>
2023-03-06 21:20:13 +0900

    [ãrfactor] ê¸view 수정

--- easytask/src/main/java/com/easytask/easytask/src/review/ReviewController.java
+++ easytask/src/main/java/com/easytask/easytask/src/review/ReviewController.java
@@ -5,6 +5,7 @@ import com.easytask.easytask.common.response.BaseResponse;
 import com.easytask.easytask.common.response.BaseResponseStatus;
 import com.easytask.easytask.src.review.dto.*;
 import com.easytask.easytask.src.review.entity.PersonalAbility;
+import com.easytask.easytask.src.review.entity.Rating;
 import com.easytask.easytask.src.review.entity.Review;
 import com.easytask.easytask.src.review.repository.PersonalAbilityRepository;
 import com.easytask.easytask.src.review.repository.ReviewRepository;
@@ -24,6 +25,7 @@ import com.easytask.easytask.src.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -59,17 +61,40 @@ public class ReviewController {
 
     @ResponseBody
     @GetMapping("/lists/users/{userId}")
-    public BaseResponse<List<Review>> getReviewsByUserId(@PathVariable("userId") Long userId) {
+    public BaseResponse<List<PostReviewRes>> getReviewsByUserId(@PathVariable("userId") Long userId) {
         List<Review> reviewList = reviewService.getReviewsByUserId(userId);
-        return new BaseResponse<>(reviewList);
+        List<PostReviewRes> list = new ArrayList<>();
+
+        for (Review review : reviewList) {
+            PostReviewRes postReviewRes = new PostReviewRes(review);
+
+            for (Rating rating: review.getRatingList()) {
+                PostRatingRes postRatingRes = new PostRatingRes(rating);
+                postReviewRes.addPostRatingRes(postRatingRes);
+            }
+            list.add(postReviewRes);
+        }
+
+        return new BaseResponse<>(list);
     }
 
     @ResponseBody
     @GetMapping("/lists/irumies/{irumiId}")
-    public BaseResponse<List<Review>> getReviewsByIrumiId(@PathVariable("irumiId") Long irumiId) {
+    public BaseResponse<List<PostReviewRes>> getReviewsByIrumiId(@PathVariable("irumiId") Long irumiId) {
         List<Review> reviewList = reviewService.getReviewsByIrumiId(irumiId);
+        List<PostReviewRes> list = new ArrayList<>();
 
-        return new BaseResponse<>(reviewList);
+        for (Review review : reviewList) {
+            PostReviewRes postReviewRes = new PostReviewRes(review);
+
+            for (Rating rating: review.getRatingList()) {
+                PostRatingRes postRatingRes = new PostRatingRes(rating);
+                postReviewRes.addPostRatingRes(postRatingRes);
+            }
+            list.add(postReviewRes);
+        }
+
+        return new BaseResponse<>(list);
     }
 
     @ResponseBody
